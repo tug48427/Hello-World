c76119285ce269db06d55874136825e326afda2d
tug48427 <tug48427@temple.edu>
2019-09-14 14:30:56 -0400

    Added a button and centered it changed the button's click behavior to change the textView's text attribute

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,12 +3,29 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.MyButton);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                TextView newTextView =  findViewById(R.id.textView);
+                newTextView.setText(R.string.newMessage);
+            }
+        });
     }
+
+
+
 }
